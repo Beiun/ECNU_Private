72552be51c0a7f904d85c5586f9564c63cf2bb71
Beiun <a308033@163.com>
2020-09-06 09:51:48 -0700

    Try

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: Sun 06 Sep 2020 09:45:24 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello, World!\r\n");
+}
